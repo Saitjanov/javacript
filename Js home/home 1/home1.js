e07b22ab0ec6a8ad752d547c82096b216dcf5c49
Saitjanov <saitjanovtimur@gmail.com>
2022-07-22 21:09:37 +0500

    fix

---
+++ Js home/home 1/home1.js
@@ -0,0 +1,26 @@
+let name = 'Джон'
+let admin = 'Джон'
+console.log(admin);
+
+let balance = 25000
+console.log(balance);
+
+let car = 4480
+let food = 890
+
+console.log(
+     car+food
+);
+
+let invoice = 5500
+let stock = 4200
+
+console.log(
+    invoice+stock
+);
+
+let totalBalance = 0
+
+console.log(
+    balance - car - food + invoice + stock
+);
---
+++ Js home/home 2/home2.js
@@ -0,0 +1,52 @@
+let name = 'Alex'
+let money = 10000
+let account = 7777
+
+let a = prompt('Whats ur name')
+
+if (a === name) {
+    let aa = prompt('Номер счета')
+} else if (aa === money){
+    let aaa = prompt('Сколько обналичть')
+}
+else{
+    alert('Пользователь не найден')
+}
+
+if (aaa === money) {
+    alert('Все отлично')
+} else{
+    alert('Недостаточно средств')
+}
+
+
+
+let name2 = prompt('Как тебя звать')
+
+if(name2.charAt() === 'A'){
+} else if (name2.charAt() === 'a'){
+} else{
+    alert('Прости но тебя не звали')
+}
+
+let club = prompt('Сколько тебе лет')
+
+if(club >= 20 && club <= 40 ) {
+} else {
+    alert('Прости но тебя не звали')
+}
+
+let cesh = prompt('сколько у тебе денег')
+
+if(cesh >= 100) {
+} else{
+    alert('Прости но тебя не звали')
+}
+
+let friend = prompt('сколько у тебя друзей')
+
+if(friend <= 8) {
+    alert('Заходи')
+}else{
+    alert('Прости но тебя не звали')
+}
